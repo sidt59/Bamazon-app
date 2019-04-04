e9a9e8fae62bc7a12052bfd321edf56f6591e6ee
STS 59 <notippin@gmail.com>
2019-04-04 00:32:38 -0500

    questions up and running, table lookin good too

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mySQL = require('mysql');
+var inquirer = require('inquirer');
 
 // connection credentials and...other...stuff?
 var connection = mySQL.createConnection({
@@ -9,8 +10,47 @@ var connection = mySQL.createConnection({
     database: 'bamazon'   
 });
 
-// let me know if you connect
+// connection test
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    showStuff();
   });
+
+// display stuff from bamazon database
+function showStuff() {
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        console.table(res);
+        questionFirst();
+    })
+}
+
+// how many products to buy
+function questionFirst() {
+    inquirer
+        .prompt([
+            {
+            /* Pass your questions in here */
+            type: 'input',
+            name: 'item_id',
+            message: 'What is the ID of the item you would like to buy?'
+            },
+            {
+            type: 'input',
+			name: 'quantity',
+            message: 'How many do you need?'
+            }
+        ])
+        .then(function(answer) {
+            // how tf do i do this?
+            var query = "SELECT item_id FROM products WHERE ?";
+                // error here...somewhere...
+                connection.query(query, { item_id: item }, function(err, res) {
+                    console.log(res);
+                })
+            });
+    connection.end();
+}
+
+// "How many units would you like to buy?"
